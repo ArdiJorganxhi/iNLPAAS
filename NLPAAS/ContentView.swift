73fef13e438cb3d754718f46fd171c2768d73078
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2024-02-07 22:33:42 +0100

    Initial Commit

---
+++ NLPAAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NLPAAS
+//
+//  Created by Ardi Jorganxhi on 7.2.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NLPAAS/NLPAASApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPAASApp.swift
+//  NLPAAS
+//
+//  Created by Ardi Jorganxhi on 7.2.24.
+//
+
+import SwiftUI
+
+@main
+struct NLPAASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
